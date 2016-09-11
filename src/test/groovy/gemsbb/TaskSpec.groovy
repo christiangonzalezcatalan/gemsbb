3de0c7b1b4b1ad06d5a3b13ce03d359b64559029
Christian González <senorshishio@hotmail.com>
2016-09-11 00:03:22 -0300

    Corrección en tests de dominio.

--- src/test/groovy/gemsbb/TaskSpec.groovy
+++ src/test/groovy/gemsbb/TaskSpec.groovy
@@ -3,6 +3,7 @@ package gemsbb
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import org.bson.types.ObjectId
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
@@ -18,42 +19,55 @@ class TaskSpec extends Specification {
 
     def 'nullable values'() {
         when: 'null name'
-        def t = new Task(responsible: new Member(name: 'christian', email: 'christian@gonzalez.cl'),
+        def t = new Task(taskId: new ObjectId(),
+                responsible: new Member(name: 'christian', email: 'christian@gonzalez.cl'),
                 startDate: new Date(), dueDate: new Date(), status: 'Finished')
 
         then: 'validation should fail'
         !t.validate()
 
         when: 'null responsible'
-        t = new Task(name: 'Christian', startDate: new Date(), dueDate: new Date(), status: 'Finished')
+        t = new Task(taskId: new ObjectId(), name: 'Christian',
+                startDate: new Date(), dueDate: new Date(), status: 'Finished')
 
         then: 'validation should fail'
         !t.validate()
 
         when: 'null startDate'
-        t = new Task(responsible: new Member(name: 'christian', email: 'christian@gonzalez.cl'),
+        t = new Task(taskId: new ObjectId(),
+                responsible: new Member(name: 'christian', email: 'christian@gonzalez.cl'),
                 name: 'Christian', dueDate: new Date(), status: 'Finished')
 
         then: 'validation should fail'
         !t.validate()
 
         when: 'null dueDate'
-        t = new Task(responsible: new Member(name: 'christian', email: 'christian@gonzalez.cl'),
+        t = new Task(taskId: new ObjectId(),
+                responsible: new Member(name: 'christian', email: 'christian@gonzalez.cl'),
                 name: 'Christian', startDate: new Date(), status: 'Finished')
 
         then: 'validation should fail'
         !t.validate()
 
         when: 'null status'
-        t = new Task(responsible: new Member(name: 'christian', email: 'christian@gonzalez.cl'),
+        t = new Task(taskId: new ObjectId(),
+                responsible: new Member(name: 'christian', email: 'christian@gonzalez.cl'),
                 name: 'Christian', startDate: new Date(), dueDate: new Date())
 
         then: 'validation should fail'
         !t.validate()
 
-        when: 'null dueDate'
+        when: 'null taskId'
         t = new Task(responsible: new Member(name: 'christian', email: 'christian@gonzalez.cl'),
                 name: 'Christian', startDate: new Date(), dueDate: new Date(), status: 'Finished')
+
+        then: 'validation should fail'
+        !t.validate()
+
+        when: 'valid task'
+        t = new Task(responsible: new Member(name: 'christian', email: 'christian@gonzalez.cl'),
+                name: 'Christian', startDate: new Date(), dueDate: new Date(), status: 'Finished',
+                taskId: new ObjectId())
 
         then: 'validation should pass'
         t.validate()
